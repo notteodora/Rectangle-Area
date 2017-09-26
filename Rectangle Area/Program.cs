51148abe74a208d34b5bfdcc1cf8ae7f4c5e6f49
Teodora Balabanova <notteodora@yahoo.com>
2017-09-26 19:31:59 +0300

    Add files via upload

---
+++ Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double sum = height * width;
+
+            Console.WriteLine("{0:f2}", sum);
+
+        }
+    }
+}
